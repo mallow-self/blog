8d7b7798696f56778af96f3059352bd2ec0b9f71
Rishikesh <rishikesh2511990@gmail.com>
2025-03-31 10:46:49 +0530

    update: model,admin done

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog
+
 
 # Register your models here.
+@admin.register(Blog)
+class BlogModel(admin.ModelAdmin):
+    list_display = ["title", "content", "image", "category"]
--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+# Add type hints for remaining fields
+class Blog(models.Model):
+    CATEGORY_CHOICES: list[tuple[str,str]] = [("python", "Python"), ("django", "Django"),
+                        ("powerbi", "PowerBI"), ("scrapy", "Scrapy")]
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to="uploads/")
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
