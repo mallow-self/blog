72fbc9a4029abfb7f187bb980d61374568c715bf
Rishikesh <rishikesh2511990@gmail.com>
2025-03-31 15:27:31 +0530

    update: action and id done

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Blog(models.Model):
     CATEGORY_CHOICES: list[tuple[str,str]] = [("python", "Python"), ("django", "Django"),
                         ("powerbi", "PowerBI"), ("scrapy", "Scrapy")]
+    
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to="uploads/")
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from asgiref.sync import sync_to_async
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, TemplateView, DetailView, UpdateView
 from .models import Blog
 from ajax_datatable.views import AjaxDatatableView
+from django.template.response import TemplateResponse
 
 # Create your views here.
 # class BlogListView(ListView):
@@ -30,12 +31,8 @@ class BlogAjaxDatatableView(AjaxDatatableView):
     model = Blog
     title = "Blogs"
     search_fields = ["title", "content", "category"]
-    columns = [
-        "title",
-        "content",
-        "category",
-    ]
     column_defs = [
+        {"name": "id", "title": "id", "visible": True, "orderable": True},
         {"name": "title", "title": "Title", "orderable": True},
         {"name": "content", "title": "Content", "orderable": False},
         {"name": "category", "title": "Category", "orderable": True},
@@ -47,12 +44,5 @@ class BlogDetailView(DetailView):
     template_name = "blog_app/detail_base.html"
     context_object_name = "detail_data"
     http_method_names = ["get"]
-    view_is_async = False
 
-    # async def get(self, request, *args, **kwargs):
-    #     """Function to make the view async"""
-        
-    #     # blog_queryset = await sync_to_async(list)(Blog.objects.all())
-    #     # if request.headers.get("X-Requested-With") == "XMLHttpRequest":
-    #     #     return JsonResponse({"blogs": [blog.to_dict() for blog in blog_queryset]})
-    #     return super().get(request, *args, **kwargs)  # Default template rendering
+
