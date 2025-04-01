7542ee173d904e3c1b151f69afe4486b35aec3e3
Rishikesh <rishikesh2511990@gmail.com>
2025-04-01 10:58:07 +0530

    add: docstring

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -10,14 +10,29 @@ from django.template.loader import render_to_string
     
 
 class BlogTableView(TemplateView):
-    template_name = "blog_app/blog_list.html"
+    """
+    A view for displaying the list of blog posts.
+
+    Attributes:
+        template_name (str): The path to the template used for rendering the blog list.
+    """
+    template_name: str = "blog_app/blog_list.html"
 
 
 class BlogAjaxDatatableView(AjaxDatatableView):
+    """
+    A view for handling AJAX-based DataTable rendering for Blog entries.
+
+    Attributes:
+        model (Model): The Django model associated with the DataTable (Blog).
+        title (str): The title displayed in the DataTable.
+        search_fields (list[str]): Fields that support search functionality.
+        column_defs (list[object]): Configuration for table columns, including visibility and orderability.
+    """
     model = Blog
-    title = "Blogs"
-    search_fields = ["title", "content", "category"]
-    column_defs = [
+    title: str = "Blogs"
+    search_fields: list[str] = ["title", "content", "category"]
+    column_defs: list[object] = [
         {"name": "id", "title": "id", "visible": True, "orderable": True},
         {"name": "title", "title": "Title", "orderable": True},
         {"name": "content", "title": "Content", "orderable": False},
@@ -26,18 +41,45 @@ class BlogAjaxDatatableView(AjaxDatatableView):
 
 
 class BlogDetailView(DetailView):
+    """
+    A view for displaying the details of a specific blog post.
+
+    Attributes:
+        model (Model): The Django model associated with this view (Blog).
+        template_name (str): The template used to render the blog detail page.
+        context_object_name (str): The name of the object passed to the template context.
+        http_method_names (list[str]): Allowed HTTP methods for this view.
+    """
     model = Blog
-    template_name = "blog_app/detail_base.html"
-    context_object_name = "detail_data"
-    http_method_names = ["get"]
+    template_name: str = "blog_app/detail_base.html"
+    context_object_name: str = "detail_data"
+    http_method_names: list[str] = ["get"]
 
 
-# update and create
 
 class BlogCreateView(CreateView):
+    """
+    A view for creating a new blog post with support for AJAX requests.
+
+    Attributes:
+        model (Model): The Django model associated with this view (Blog).
+        fields (list[str]): The fields to be displayed in the form.
+        template_name (str): The template used to render the form.
+        success_url (str): The URL to redirect to after a successful submission.
+
+    Methods:
+        get(request, *args, **kwargs):
+            Handles GET requests. If the request is AJAX, returns only the form.
+        
+        form_valid(form):
+            Handles successful form submission. Returns JSON response for AJAX requests.
+        
+        form_invalid(form):
+            Handles form submission with errors. Returns JSON response with form errors for AJAX requests.
+    """
     model = Blog
-    fields = ['title', 'content', 'image', 'category']
-    template_name = 'blog_app/blog_form_partial.html'
+    fields: list[str] = ['title', 'content', 'image', 'category']
+    template_name: str = 'blog_app/blog_form_partial.html'
     success_url = reverse_lazy('blog:blog_list')
 
     def get(self, request, *args, **kwargs):
@@ -69,9 +111,29 @@ class BlogCreateView(CreateView):
 
 
 class BlogUpdateView(UpdateView):
+    """
+    A view for updating an existing blog post with support for AJAX requests.
+
+    Attributes:
+        model (Model): The Django model associated with this view (Blog).
+        fields (list[str]): The fields to be displayed in the update form.
+        template_name (str): The template used to render the update form.
+        success_url (str): The URL to redirect to after a successful update.
+
+    Methods:
+        get(request, *args, **kwargs):
+            Handles GET requests. If the request is AJAX, returns only the form along with existing data.
+
+        form_valid(form):
+            Handles successful form submission. If no new image is uploaded and an existing image is present, 
+            it retains the current image. Returns JSON response for AJAX requests.
+
+        form_invalid(form):
+            Handles form submission with errors. Returns JSON response with form errors for AJAX requests.
+    """
     model = Blog
-    fields = ['title', 'content', 'image', 'category']
-    template_name = 'blog_app/blog_form_partial.html'
+    fields: list[str] = ['title', 'content', 'image', 'category']
+    template_name: str = 'blog_app/blog_form_partial.html'
     success_url = reverse_lazy('blog:blog_list')
 
     def get(self, request, *args, **kwargs):
@@ -98,7 +160,6 @@ class BlogUpdateView(UpdateView):
             if not self.request.FILES.get('image') and 'image-clear' not in self.request.POST:
                 # Keep the existing image
                 self.object = form.save(commit=False)
-
                 # Get the existing Blog object and its image
                 existing_blog = get_object_or_404(Blog, pk=self.object.pk)
                 self.object.image = existing_blog.image
@@ -135,6 +196,16 @@ class BlogUpdateView(UpdateView):
 
 
 class BlogDeleteView(DeleteView):
+    """
+    A view for deleting a blog post with AJAX support.
+
+    Attributes:
+        model (Model): The Django model associated with this view (Blog).
+
+    Methods:
+        post(request, *args, **kwargs):
+            Handles AJAX-based deletion of a blog post. Returns a JSON response upon success.
+    """
     model = Blog
 
     def post(self, request, *args, **kwargs):
@@ -144,5 +215,3 @@ class BlogDeleteView(DeleteView):
             'success': True,
             'message': 'Blog deleted successfully!'
         })
-    
-    # success_url = reverse_lazy('blog:blog_list')
